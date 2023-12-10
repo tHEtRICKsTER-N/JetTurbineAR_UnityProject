ff2ad5ee7c054e33863fc15bf9782ed89d56df56
Naimish <rishunaimish@gmail.com>
2023-12-10 11:19:35 +0530

    Added Sliders to control fan speed and sound

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,13 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rotator : MonoBehaviour
 {
     [SerializeField] private Vector3 _rotationVector;
+    [SerializeField] private Slider _slider;
+
+    AudioSource _aSource;
+
+    private void Start()
+    {
+        _aSource = GetComponent<AudioSource>();
+
+        _slider.minValue = 0;
+        _slider.maxValue = 1000f;
+        _slider.value = 100f;
+    }
 
     private void Update()
     {
-        transform.Rotate(_rotationVector * Time.deltaTime);   
+        _rotationVector.y = _slider.value;
+        transform.Rotate(_rotationVector * Time.deltaTime);
+        _aSource.volume = _slider.value / 1000f;
     }
 }
